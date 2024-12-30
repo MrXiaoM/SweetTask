6847add9e9145dfde96f251cd2aa84213a7c7f35
人間工作 <coolxiaom95@gmail.com>
2024-12-30 16:27:06 +0800

    task and sub task operation

--- src/main/java/top/mrxiaom/sweet/taskplugin/database/TaskProcessDatabase.java
+++ src/main/java/top/mrxiaom/sweet/taskplugin/database/TaskProcessDatabase.java
@@ -13,11 +13,13 @@ import org.jetbrains.annotations.Nullable;
 import top.mrxiaom.pluginbase.database.IDatabase;
 import top.mrxiaom.sweet.taskplugin.SweetTask;
 import top.mrxiaom.sweet.taskplugin.func.AbstractPluginHolder;
+import top.mrxiaom.sweet.taskplugin.func.entry.LoadedTask;
+import top.mrxiaom.sweet.taskplugin.tasks.IDataHolder;
+import top.mrxiaom.sweet.taskplugin.tasks.ITask;
 
 import java.sql.*;
 import java.time.LocalDateTime;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
 public class TaskProcessDatabase extends AbstractPluginHolder implements IDatabase, Listener {
     public static class TaskCache {
@@ -157,5 +159,78 @@ public class TaskProcessDatabase extends AbstractPluginHolder implements IDataba
         } catch (SQLException e) {
             warn(e);
         }
+    }
+
+    public void addTask(Player player, LoadedTask task, LocalDateTime expireTime) {
+        String id = id(player);
+        try (Connection conn = plugin.getConnection()) {
+            List<String> list = new ArrayList<>();
+            list.add(task.id);
+            for (Map.Entry<String, ITask> entry : task.subTasks.entrySet()) {
+                String subTaskId = entry.getKey();
+                ITask subTask = entry.getValue();
+                if (subTask instanceof IDataHolder) {
+                    list.add(subTaskId);
+                }
+            }
+            try (PreparedStatement ps = conn.prepareStatement(
+                    "INSERT INTO `" + TABLE_NAME + "`" +
+                            "(`player`, `task_id`, `sub_task_id`, `data`, `expire_time`) " +
+                            "VALUES(?, ?, ?, 0, ?);")) {
+                for (String s : list) {
+                    ps.setString(1, id);
+                    ps.setString(2, task.id);
+                    ps.setString(3, s);
+                    ps.setTimestamp(4, Timestamp.valueOf(expireTime));
+                    ps.addBatch();
+                }
+                ps.executeBatch();
+            }
+        } catch (SQLException e) {
+            warn(e);
+        }
+    }
+
+    public void submitCache(Player player) {
+        String id = id(player);
+        Map<String, TaskCache> cache = caches.get(id);
+        if (cache == null) return;
+        String sentence;
+        boolean mysql = plugin.options.database().isMySQL();
+        if (mysql) {
+            sentence = "INSERT INTO `" + TABLE_NAME + "`" +
+                    "(`player`, `task_id`, `sub_task_id`, `data`, `expire_time`) " +
+                    "VALUES(?, ?, ?, ?, ?) " +
+                    "on duplicate key update `data`=?;";
+        } else if (plugin.options.database().isSQLite()) {
+            sentence = "INSERT OR REPLACE INTO `" + TABLE_NAME + "`" +
+                    "(`player`, `task_id`, `sub_task_id`, `data`, `expire_time`) " +
+                    "VALUES(?, ?, ?, ?, ?);";
+        } else return;
+        try (Connection conn = plugin.getConnection();
+            PreparedStatement ps = conn.prepareStatement(sentence)) {
+            for (TaskCache taskCache : cache.values()) {
+                addBatchCache(ps, mysql, id, taskCache.taskId,
+                        taskCache.taskId, 0, taskCache.expireTime);
+                for (Map.Entry<String, Integer> entry : taskCache.subTaskData.entrySet()) {
+                    addBatchCache(ps, mysql, id, taskCache.taskId,
+                            entry.getKey(), entry.getValue(), taskCache.expireTime);
+                }
+            }
+            ps.executeBatch();
+        } catch (SQLException e) {
+            warn(e);
+        }
+    }
+    private static void addBatchCache(PreparedStatement ps, boolean mysql, String player, String task, String subTask, int data, LocalDateTime expire) throws SQLException {
+        LocalDateTime now = LocalDateTime.now();
+        if (now.isAfter(expire)) return;
+        ps.setString(1, player);
+        ps.setString(2, task);
+        ps.setString(3, subTask);
+        ps.setInt(4, data);
+        ps.setTimestamp(5, Timestamp.valueOf(expire));
+        if (mysql) ps.setInt(6, data);
+        ps.addBatch();
     }
 }
---
+++ src/main/java/top/mrxiaom/sweet/taskplugin/tasks/ITask.java
@@ -0,0 +1,4 @@
+package top.mrxiaom.sweet.taskplugin.tasks;
+
+public interface ITask {
+}
