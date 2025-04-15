50e97e3c3987a925063caa4c2493848544a00cb0
人間工作 <coolxiaom95@gmail.com>
2025-04-15 21:45:15 +0800

    upgrade dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,10 +36,10 @@ dependencies {
     implementation("net.kyori:adventure-api:4.17.0")
     implementation("net.kyori:adventure-platform-bukkit:4.3.4")
     implementation("net.kyori:adventure-text-minimessage:4.17.0")
-    implementation("de.tr7zw:item-nbt-api:2.14.0")
+    implementation("de.tr7zw:item-nbt-api:2.14.2-SNAPSHOT")
     implementation("com.zaxxer:HikariCP:4.0.3") { isTransitive = false }
     implementation("org.jetbrains:annotations:24.0.0")
-    implementation("top.mrxiaom:PluginBase:1.3.6")
+    implementation("top.mrxiaom:PluginBase:1.3.8")
 }
 java {
     val javaVersion = JavaVersion.toVersion(targetJavaVersion)
