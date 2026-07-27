d630093236480c82e02408fcac55e0f22f1a3453
MrXiaoM <coolxiaom95@gmail.com>
2026-07-27 14:40:00 +0800

    upgrade dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 
 buildscript {
     repositories.mavenCentral()
-    dependencies.classpath("top.mrxiaom:LibrariesResolver-Gradle:1.7.29")
+    dependencies.classpath("top.mrxiaom:LibrariesResolver-Gradle:1.7.30")
 }
 val base = LibraryHelper(project)
 
