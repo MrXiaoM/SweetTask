01176d0ee8c2cefaa6521de5ea57301650b50d16
MrXiaoM <coolxiaom95@gmail.com>
2026-07-31 09:44:55 +0800

    update 1.0.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -51,7 +51,7 @@ dependencies {
     base.library(base.depend.HikariCP)
     base.collectPluginHolders()
 
-    implementation("de.tr7zw:item-nbt-api:2.15.8-SNAPSHOT")
+    implementation("de.tr7zw:item-nbt-api:2.16.0")
     implementation("com.github.technicallycoded:FoliaLib:0.4.4") { isTransitive = false }
     for (artifact in pluginBaseModules) {
         implementation(artifact)
